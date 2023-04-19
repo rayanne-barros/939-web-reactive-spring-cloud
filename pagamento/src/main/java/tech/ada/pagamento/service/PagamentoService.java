2e36e9e0e0858631f686085dbbf2c7f989d53229
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2023-04-19 14:04:55 -0300

    setup inicial heroku

--- pagamento/src/main/java/tech/ada/pagamento/service/PagamentoService.java
+++ pagamento/src/main/java/tech/ada/pagamento/service/PagamentoService.java
@@ -28,6 +28,14 @@ public class PagamentoService {
                         .build())
                 .retrieve().bodyToFlux(Usuario.class);
 
+        usuarios.flatMap(u -> {
+            log.error(u.getUsername() + " : " + u.getBalance());
+            return null;
+        }).subscribe(u -> {
+            log.error(u.getUsername() + " : " + u.getBalance());
+            return null;
+        });
+
         Mono<Comprovante> comprovanteMono = Flux.zip(usuarios, usuarios.skip(1))
                 .map(tupla -> new Transacao(
                         tupla.getT1().getUsername(),
